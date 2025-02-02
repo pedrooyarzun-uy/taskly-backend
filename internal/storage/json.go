f0ba7c94d18e4d2042d41111d19d521a7eaf7ec0
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2025-02-02 17:39:40 -0300

    If data.json does not exist cretes files

--- internal/storage/json.go
+++ internal/storage/json.go
@@ -10,9 +10,41 @@ import (
 const FILE_NAME = "data.json"
 const PATH = "../../internal/storage/" + FILE_NAME
 
+func CheckFileExistance() error {
+	file, err := os.OpenFile(PATH, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	status, err := file.Stat()
+
+	if err != nil {
+		return err
+	}
+
+	if status.Size() == 0 {
+		_, err := file.Write([]byte("[]"))
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+
+}
+
 func LoadTasks() []models.Task {
 
 	var tasks []models.Task
+
+	//Checks if file exists
+	existance := CheckFileExistance()
+
+	if existance != nil {
+		panic(existance)
+	}
 
 	data, err := os.ReadFile(PATH)
 	if err != nil {
