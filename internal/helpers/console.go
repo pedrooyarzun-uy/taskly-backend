e6b674416cec4fecb1a4c7ffaf6b7430b0cfa013
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2025-03-19 21:44:25 -0300

    Fix for cleaning console on linux/mac

--- internal/helpers/console.go
+++ internal/helpers/console.go
@@ -3,10 +3,19 @@ package helpers
 import (
 	"os"
 	"os/exec"
+	"runtime"
 )
 
 func ConsoleCleaner() {
-	c := exec.Command("cmd", "/c", "cls")
-	c.Stdout = os.Stdout
-	c.Run()
+
+	var cmd *exec.Cmd
+
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	} else {
+		cmd = exec.Command("clear")
+	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
