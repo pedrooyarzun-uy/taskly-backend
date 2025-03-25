9c243311e7ecb2645ab7c9341daa9af0a0eb0ac4
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2025-03-25 19:32:36 -0300

    Added singleton for database

--- internal/db/db.go
+++ internal/db/db.go
@@ -6,30 +6,34 @@ import (
 	_ "github.com/lib/pq"
 	"log"
 	"os"
+	"sync"
 )
 
 var DB *sqlx.DB
+var once sync.Once
 
 func Init() {
-	host := os.Getenv("DB_URL")
-	dbName := os.Getenv("DB_NAME")
-	username := os.Getenv("DB_USERNAME")
-	password := os.Getenv("DB_PASSWORD")
-	port := os.Getenv("DB_PORT")
+	once.Do(func() {
+		host := os.Getenv("DB_URL")
+		dbName := os.Getenv("DB_NAME")
+		username := os.Getenv("DB_USERNAME")
+		password := os.Getenv("DB_PASSWORD")
+		port := os.Getenv("DB_PORT")
 
-	conString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		host, port, username, password, dbName,
-	)
+		conString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			host, port, username, password, dbName,
+		)
 
-	DB, err := sqlx.Open("postgres", conString)
+		DB, err := sqlx.Open("postgres", conString)
 
-	if err != nil {
-		log.Fatal("String connection failed: ", err)
-	}
+		if err != nil {
+			log.Fatal("String connection failed: ", err)
+		}
 
-	if err = DB.Ping(); err != nil {
-		log.Fatal("Ping didn't work: ", err)
-	}
+		if err = DB.Ping(); err != nil {
+			log.Fatal("Ping didn't work: ", err)
+		}
+	})
 }
 
 func Close() {
