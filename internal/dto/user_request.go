55e412ea3d904135225b23cc07122823418e86b0
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2025-05-22 18:19:08 -0300

    Added new route. Added password hasing function

---
+++ internal/dto/user_request.go
@@ -0,0 +1,7 @@
+package dto
+
+type CreateUserRequest struct {
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
--- internal/routes/user_routes.go
+++ internal/routes/user_routes.go
@@ -1,13 +1,22 @@
 package routes
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
+	"todo-app/internal/dto"
 	"todo-app/internal/service"
 )
 
 func RegisterUserRoutes(r *gin.RouterGroup, s service.UserService) {
 	r.POST("/sign-up", func(c *gin.Context) {
-		fmt.Println(c.ShouldBind())
+
+		var req dto.CreateUserRequest
+
+		if err := c.BindJSON(&req); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		err = s.CreateUser()
+
 	})
 }
