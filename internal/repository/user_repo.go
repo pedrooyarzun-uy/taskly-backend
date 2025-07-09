4b9d033ff4027f65965afb0cc692f98cf3b5cb11
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2025-07-08 21:53:31 -0300

    Fix on user repo and girignore

--- internal/repository/user_repo.go
+++ internal/repository/user_repo.go
@@ -3,14 +3,13 @@ package repository
 import (
 	"database/sql"
 	"fmt"
-	"github.com/jmoiron/sqlx"
 	"todo-app/internal/domain"
+
+	"github.com/jmoiron/sqlx"
 )
 
 type UserRepository interface {
 	CreateUser(user domain.User) error
-	GetAllPendingTasks(id int) ([]domain.Task, error)
-	GetAllTasks(id int) ([]domain.Task, error)
 	UserExists(email string) bool
 	GetUserByEmail(email string) (*domain.User, error)
 }
@@ -33,30 +32,6 @@ func (r *userRepository) CreateUser(user domain.User) error {
 
 	return nil
 
-}
-
-func (r *userRepository) GetAllPendingTasks(userId int) ([]domain.Task, error) {
-	tasks := []domain.Task{}
-
-	err := r.db.Select(&tasks, "SELECT * FROM task WHERE user_id = $1 AND completed = false", userId)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return tasks, nil
-}
-
-func (r *userRepository) GetAllTasks(userId int) ([]domain.Task, error) {
-	tasks := []domain.Task{}
-
-	err := r.db.Select(&tasks, "SELECT * FROM task WHERE user_id = $1", userId)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return tasks, nil
 }
 
 func (r *userRepository) UserExists(email string) bool {
