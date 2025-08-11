6b7135456df2a5790dca01021e35e4ec2d4b5598
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2025-08-11 20:31:48 -0300

    Fix para listar las categorias con su total de items

--- internal/domain/category.go
+++ internal/domain/category.go
@@ -6,3 +6,9 @@ type Category struct {
 	User    int    `db:"user_id"`
 	Deleted bool   `db:"deleted"`
 }
+
+type AllCategories struct {
+	Id         int    `db:"id"`
+	Name       string `db:"name"`
+	TotalTasks int    `db:"total_tasks"`
+}
